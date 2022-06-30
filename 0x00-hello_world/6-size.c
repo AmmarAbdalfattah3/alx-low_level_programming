51f0d0766a9c8ffa1aa7dba2a75dc3b682e20c2b
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-06-30 14:47:28 -0700

    the size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+     int A;
+     long int B;
+     long long int C;
+     char D;
+    
+int main(void)
+{
+	printf("Size of an int: %zu bytes\n", sizeof(A));
+	printf("Size of a long int: %zu bytes\n", sizeof(B));
+	printf("Size of a long long int: %zu bytes\n", sizeof(C));
+	printf("Size of a char: %zu byte\n", sizeof(d));
+
+	retun (0);
+
+}
+

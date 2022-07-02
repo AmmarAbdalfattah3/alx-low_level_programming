0b4f619fcac5efe4745ceca0b9362a7c879235f4
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-01 19:38:43 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints "alphabet in lowercase"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
+

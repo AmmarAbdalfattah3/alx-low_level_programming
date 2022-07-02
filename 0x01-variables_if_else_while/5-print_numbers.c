2bc5cae68568f39bfbf0568d6a63c490b161d22d
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-01 19:34:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * main - prints "alphabet in lowercase"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+for (x = 0; x<= 9; x++)
+{
+printf("%i", x);
+}
+putchar('\n');
+return (0);
+}

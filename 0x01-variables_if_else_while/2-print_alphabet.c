0bf85b8f9131811435e02e818cc319db00394e56
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-01 19:08:32 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints "alphabet in lowercase"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+
+}
+putchar('\n');
+return (0);
+}

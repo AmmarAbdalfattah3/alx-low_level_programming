3d271c49b1b69cffc89058190dc12c0cd3310cda
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-05 14:02:02 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,14 @@ if (n > 0)
 _putchar(+);
 return (1);
 }
-while (n = 0)
-{
-_putchar(0);
-return (0);
-}
-else
+else if (n < 0)
 {
 _putchar(-);
 return (-1);
+}
+else
+{
+_putchar(0);
+return (0);
 }
 }

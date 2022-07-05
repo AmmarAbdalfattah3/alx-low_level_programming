e6b26fcea92caef77c3ccfb9021fbe1b85c04da3
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-05 08:56:10 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+int _putchar(int c);
 /**
  * main- entry Point
  *
@@ -5,13 +6,13 @@
  **/
 int main(void)
 {
-_putchar(_);
-_putchar(p);
-_putchar(u);
-_putchar(t);
-_putchar(c);
-_putchar(h);
-_putchar(a);
-_putchar(r);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }

4e943edb11bb127d70941ec80e6b4f20668b44e8
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-06 13:59:49 -0700

    addtoin two intgers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,38 +7,5 @@
  */
 int add(int l, int z)
 {
-int a;
-int j;
-int n;
-int k;
-int i;
-int v;
-if (l == -)
-{
-a = l * -1;
-j = z + a;
-_putchar(j);
-}
-else if (z == -)
-{
-n = z * -1;
-k = n + l;
-_putchar(k);
-}
-else if ((l == -) &&(z == -))
-{
-i = a + n;
-_putchar(i);
-}
-else
-{
-v = l + z;
-_putchar(v);
-}
-}
-_putchar('\n');
-return (j);
-return (k);
-return (i);
-return (v);
+return (z + l);
 }

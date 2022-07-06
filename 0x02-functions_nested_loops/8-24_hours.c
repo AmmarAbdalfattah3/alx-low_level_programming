178b5207dab1b97e9fedad79d01153d28665b10e
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-05 19:09:56 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,4 +3,19 @@
  * jack_bauer - prints every minute of the day of Jack Bauer
  */
 void jack_bauer(void)
-
+{
+int h;
+int m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
+}
+}
+}

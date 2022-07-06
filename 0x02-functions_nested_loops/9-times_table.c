428ef01e899aa87538b62545a49ca1cadc02c8e3
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-06 09:55:37 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,14 +12,14 @@ for (x = 0; x <= 9; x++)
 for (b = 0; b <= 9; b++)
 {
 p = x * b;
+if (b != 0)
+{
+_putchar(' ');
+_putchar(',');
+}
 if (b == 0)
 {
 _putchar(0);
-}
-if (b != 0)
-{
-_putchar(',');
-_putchar(' ');
 }
 else if (p >= 10)
 {

072e67ce0c87613209293ed2f49685ea64b5d48d
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-07 17:58:47 -0700

    a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: is a variable
+ */
+void print_square(int size)
+{
+{
+int c;
+int x;
+for (c = 0; c < size; c++)
+{
+for (x = 0; x < size; x++)
+{
+putchar('#');
+}
+putchar('\n');
+}
+}

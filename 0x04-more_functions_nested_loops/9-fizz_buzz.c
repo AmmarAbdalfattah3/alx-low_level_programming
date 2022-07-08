44a78bb5430712189605c61d14dafd371daa2bf7
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-08 16:45:25 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,22 +8,23 @@ int main(void)
 int n;
 for (n = 1; n <= 100; n++)
 {
-if (n / 3)
-{
-printf("Fizz");
-}
-else if (n / 5)
-{
-printf("Buzz");
-}
-else if (n / 3 && n / 5)
+if (n % 3 == 0 && n % 5 == 0)
 {
 printf("FizzBuzz");
 }
+else if (n % 3 == 0)
+{
+printf("Fizz");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz");
+}
 else
 {
-printf("n");
+printf("%i", n);
+}
+printf(" ");
 }
 printf("\n");
-}
 }

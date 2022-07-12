ea8b6b426a984d1a7c9b3f8468ab51894c510615
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-12 08:31:21 -0700

    seconed reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ * @s: is a parameter
+ */
+void rev_string(char *s)
+{
+char *k;
+k = s;
+
+while (*s != '\0')
+{
+s++;
+}
+s -= 1;
+
+while (k <= s)
+{
+s--;
+}
+_putchar('\n');
+}

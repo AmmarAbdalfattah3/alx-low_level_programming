0e8b914cb629edf68cc84d2ba31cadffae7db398
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-17 13:46:29 -0700

    uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *string_toupper - changes all lowercase letters.
+ * @n: is a parameter
+ * Return: 0
+ */
+char *string_toupper(char *n)
+{
+int h;
+
+for (h = 0; n[h + 32] != '\0'; h++)
+{
+printf("%c", n[h + 32]);
+}
+printf("\n");
+}
+

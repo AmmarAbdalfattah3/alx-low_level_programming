f783f8cf7e21a5cc4a775ac115799e52ea11d8cb
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-23 19:39:00 -0700

    argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: is a first parameter
+ * @argv: is a second parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int n1;
+int n2;
+int res;
+
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+res = n1 *n;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+printf("%d\n", res);
+return (0);
+
+}

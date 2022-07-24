2ee512170b73ff63340c090b0c3992455f36f571
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-23 19:45:47 -0700

    argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,14 @@ int n1;
 int n2;
 int res;
 
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-res = (n1)*(n2);
-
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+res = (n1)*(n2);
 printf("%d\n", res);
 return (0);
 

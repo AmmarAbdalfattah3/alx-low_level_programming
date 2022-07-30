cdb2dbd8d27e367146c6d85b910306c1b972fb0d
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-07-30 14:03:28 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two positive numbers.
+ * @argc: is a argument
+ * @argv: is a argument
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+if (argc == 3)
+{
+if (atoi(argv[1]) > 0 && atoi(argv[2]) > 0)
+{
+sum = atoi(argv[1])  * atoi(argv[2]);
+printf("%i\n", sum);
+return (0);
+}
+}
+printf("Error\n");
+exit(98);
+}

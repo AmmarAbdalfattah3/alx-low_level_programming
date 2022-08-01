4628fefa53fdaae8e115368e4295debfb7e903a5
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-01 11:02:15 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - creat a structure
+ * @name: a first variable
+ * @owner: a second variable
+ * @age: a third variable
+ */
+struct dog
+{
+char *name;
+char *owner;
+float age;
+}
+#endif

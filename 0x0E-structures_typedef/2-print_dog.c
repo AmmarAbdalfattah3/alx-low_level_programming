0714b870c2738547098f337710f72d1f14d8dd8e
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-01 18:24:42 -0700

    structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,15 +12,25 @@ if (d->name)
 {
 printf("Name:%s\n", d->name);
 }
+else
+{
 printf("Name: (nil)\n");
+}
 if (d->age >= 0)
 {
 printf("Age:%f\n", d->age);
 }
+else
+{
 printf("Age: (nil)\n");
+}
 if (d->owner)
 {
 printf("Owner:%s\n", d->owner);
 }
+else
+{
 printf("Owner: (nil)\n");
+}
+}
 }

4d8d1b2e48d52ce2216e8c243e9956c141ef0b78
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-02 16:05:52 -0700

    dog_t

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - creates a new dog.
+ * @name: is a first parameter
+ * @age: is a second parameter
+ * @owner: is a third parameter
+ * Return: the address of the struct or NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *stupid_dog;
+char *na, *ow;
+unsigned int x = 0;
+na = malloc(sizeof(char) * (strlen(name) + 1));
+if (na == NULL)
+{
+free(na);
+return (NULL);
+}
+ow = malloc(sizeof(char) * (strlen(owner) + 1));
+if (ow == NULL)
+{
+free(ow);
+return (NULL);
+}
+stupid_dog = malloc(sizeof(dog_t));
+if (stupid_dog)
+{
+while (name[x] != '\0')
+{
+na[x] = name[x];
+x++;
+}
+na[x] = '\0';
+x = 0;
+while (owner[x] != '\0')
+{
+ow[x] = owner[x];
+x++;
+}
+ow[x] = '\0';
+stupid_dog->name = na;
+stupid_dog->age = age;
+stupid_dog->owner = owner;
+return (stupid_dog);
+}
+free(na);
+free(ow);
+return (NULL);
+}

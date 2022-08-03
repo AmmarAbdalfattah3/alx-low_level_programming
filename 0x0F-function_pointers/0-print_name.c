641b2ad1c42f69a8f1519ced087cc232fcac5852
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-03 16:04:34 -0700

    function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name.
+ * @name: is a first parameter
+ * @f: is a second parameter
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}

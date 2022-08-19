999b83fa435fa302a69d43880bd8a9689aa678d0
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-19 16:22:50 -0700

    bitwise

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * get_bit - returns the value of a bit at a given index.
- * @n: is the index of the bit.
- * @index: the decimal value.
+ * @n: the decimal value.
+ * @index: is the index of the bit.
  * Return: the value of the bit at index index or -1 if an error occured.
  */
 

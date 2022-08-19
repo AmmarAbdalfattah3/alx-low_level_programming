2bb0481afe7728114169d00fd6e83ea79eb4e258
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-18 19:32:16 -0700

    bitwise

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *  print_binary - prints the binary representation of a number.
+ *  @n: the decimal number to be inserted.
+ *  Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int numb;
+	int i;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	numb = n;
+	for (i = 0; i != 0; i++)
+	{
+		n >>= 1;
+	}
+	for (i -= 1; i >= 0; i--)
+	{
+		putchar('0' +  ((numb >> i) & 1));
+	}
+}

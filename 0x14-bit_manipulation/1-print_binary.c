602923c5f5f5f952eef07f03843066228ec29f28
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-19 11:39:00 -0700

    Bitwise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,17 +10,21 @@ void print_binary(unsigned long int n)
 	int i;
 
 	numb = n;
+
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
+
 	for (i = 0; i != 0; i++)
 	{
 		n = n >> 1;
 	}
+
 	for (i -= 1; i >= 0; i--)
 	{
 		_putchar('0' + ((numb >> i) & 1));
 	}
+
 }

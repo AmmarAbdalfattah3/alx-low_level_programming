d91c81b57983b3021eab12bd08bc56cde49ba1de
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-20 08:25:27 -0700

    single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,16 +4,16 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 for (int i = '0'; i <= '9'; i++)
 {
 putchar(i);
-if (i >= '9')
-return (0);
+if (i < '9')
+{
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
+}
+return (0);
 }

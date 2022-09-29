92332280b2b2f2cc08419a65a5d91508816f8b37
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-09-29 09:45:34 -0700

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * dlistint_len - prints all the elements of a dlistint_t list.
+ * @h: pointer to a doubly linked list.
+ * Return: the number of elements.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+return (i);
+}

d169c64edfdf0c66dfa39c23d540dc88468e2c9d
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-09-29 06:50:03 -0700

    print list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef DOUBLY_LISTS_H
+#define DOUBLY_LISTS_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -8,3 +11,6 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+#endif

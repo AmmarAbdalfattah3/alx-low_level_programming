33539a0496120596090fb854a61f65226975aaec
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-09-29 20:39:17 -0700

    free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,7 +3,7 @@
  * free_dlistint - frees a dlistint_t list.
  * @head: pointer to a doubly linked list.
  */
-void free_dlistint(dlistint_t *head);
+void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *free_node;
 

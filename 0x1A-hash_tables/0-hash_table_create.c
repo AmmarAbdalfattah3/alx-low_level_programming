5d5e50665a811a552d00708cb2e4c8d63de57f35
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-10-23 12:14:37 -0700

    creating a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,6 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hash->array == NULL)
 	{
+		free(new_hash);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)

be19f29bcb0bfc057dde39c12b61c7b344012141
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-11-01 07:19:29 -0700

    Create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -23,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_hash);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < new_hash->size; i++)
 	{
 		new_hash->array[i] = NULL;
 	}

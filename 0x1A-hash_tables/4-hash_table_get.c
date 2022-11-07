44f35db187bf19e83ff56a1d3c454b41ccf8ee03
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-11-06 17:33:55 -0800

    Get a hash function

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: is the hash table.
+ * @key: is the key.
+ * Return:  the value associated with the element, or NULL if failed.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index_key = 0;
+	hash_node_t *tmp;
+
+	if (!key || !*key || !ht)
+	{
+		return (NULL);
+	}
+	index_key = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index_key];
+	while (tmp)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+}

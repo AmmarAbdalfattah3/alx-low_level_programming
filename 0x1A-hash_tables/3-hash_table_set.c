4136686e0f89498b5c1f4e67067a7fbe9cc176d8
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-03-02 04:47:42 -0800

    hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,7 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		temp2 = node;
 		node->next = ht->array[index_key];
-		ht->array[index_key];
+		ht->array[index_key] = temp2;
 	}
 	return (1);
 }

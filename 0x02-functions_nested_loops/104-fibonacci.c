70d5c0bc069bc2c33bffdba08aa0c1230d501256
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-09-02 00:25:33 -0700

    the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,17 +10,17 @@ int main(void)
 	int n = 0;
 	unsigned long int x = 1, y = 2;
 
-	while (n < 48)
+	while (n < 47)
 	{
 		printf("%lu", x);
 		x += y;
-		if (n < 47)
+		if (n < 46)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 
-		if (n <= 46)
+		if (n <= 45)
 		{
 			printf("%lu, ", y);
 			y += x;
